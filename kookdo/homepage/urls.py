1b1b05700899feee8cc42a07e365ade0e9e12f9b
Mijeong Ban <mban1@stevens.edu>
2018-06-30 17:54:09 +0900

    aboutus page setup

--- kookdo/homepage/urls.py
+++ kookdo/homepage/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about_overview/', views.about_overview, name='about_overview'),
 ]
--- kookdo/homepage/views.py
+++ kookdo/homepage/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'homepage/index.html', {})
+
+def about_overview(request):
+    return render(request, 'homepage/about_overview.html', {})

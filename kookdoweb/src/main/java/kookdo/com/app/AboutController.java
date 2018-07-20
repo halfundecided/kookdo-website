712abc5eb6e152d9f5b4e590a657a0af0e3362f1
typark <pty1026@naver.com>
2018-07-20 22:20:24 +0900

    페이지 이동 HttpServletRequest 및 HttpServletResponse를 이용하여 데이터 전송 확인 추후 mysql연동

--- kookdoweb/src/main/java/kookdo/com/app/AboutController.java
+++ kookdoweb/src/main/java/kookdo/com/app/AboutController.java
@@ -4,12 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kookdo.common.AbstractAction;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 @Controller
-public class AboutController {
+public class AboutController extends AbstractAction{
 
 	@RequestMapping(value = "about", method = RequestMethod.GET)
-	public String home() {
+	public String home(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		return execute(req, res);
+	}
+	
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		System.out.println("AboutController execute");
+		
+		req.setAttribute("msg", "msg");
+		
 		return "about";
+
 	}
 
 }
